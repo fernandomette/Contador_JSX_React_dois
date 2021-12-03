efd620b7cb9ed5bab728e7176e62ab7b6823979d
Fernando Mette <metteinfor@gmail.com>
2021-12-03 16:37:27 -0300

    tette

--- src/components/Calculadora/index.jsx
+++ src/components/Calculadora/index.jsx
@@ -14,13 +14,27 @@ export default function Contador() {
   }
 
   return (
-    <div className="wrapper">
-      <div className="contador">
-        {contador}
-        <div>
-          <button onClick={somar}>Mais</button>
+    <div className="contador">
+      {contador}
+      <div>
           <button onClick={subtarir}>Menos</button>
-        </div>
+          <h1>Sistema de Calculadora</h1>
+          
+          <input type="text" id="caixanumerodigitado" name="caixanumerodigitado" size="1" value="0">
+          <br> </br>
+
+          <button onClick={somar}>Somar (+)</button>
+          <button onClick={diminuir}>Diminuir (-)</button>
+          <button onClick={multiplicar}>Multiplicar (x)</button>
+          <button onClick={dividir}>Dividir (/)</button>
+
+          <h3>Resultado:</h3>
+          <p id="resultado">0</p>
+
+          <button onclick="zerarresultado()">Zerar Resultado</button>
+          <br> </br>
+
+        <div id="listahistorico"></div>
       </div>
     </div>
   );
