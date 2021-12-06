9633820a0d0367b309b8a907705711bc40b3edec
Fernando Mette <metteinfor@gmail.com>
2021-12-06 10:58:12 -0300

    css

--- src/components/Calculadora/index.jsx
+++ src/components/Calculadora/index.jsx
@@ -2,37 +2,116 @@ import "./index.css";
 import { useState } from "react";
 
 export default function Contador() {
-  const [contador, setContador] = useState(0);
-  const [teste, setTeste] = useState(5);
+  const [resultado, setResultado] = useState(0);
 
-  function somar() {
-    setContador(contador + teste);
+  var listadehistorico = [(listaulli = [])];
+
+  function CriaUlLI(array) {
+    var listhistoricoul = document.createElement("ul");
+
+    var item = document.createElement("li");
+
+    for (var i = 0; i < array.length; i++) {
+      item.appendChild(document.createTextNode(array[i]));
+      listhistoricoul.appendChild(item);
+    }
+
+    // Retorne a lista construída:
+    return listhistoricoul;
   }
 
-  function subtarir() {
-    setTeste(teste - contador);
+  function somar() {
+    var numeroDigitado = 0; //Valor da caixa digitado
+
+    var valorResultadoanterior = resultado;
+
+    setResultado(resultado + numeroDigitado);
+
+    listaulli.splice(0, qualquer.length);
+
+    listaulli.push(valoranterior, " + ", numeroDigitado, " = ", resultado);
+
+    document
+      .getElementById("listahistorico")
+      .appendChild(CriaUlLI(listadehistorico[0]));
+  }
+
+  function diminuir() {
+    var numeroDigitado = 0;
+
+    var valorResultadoanterior = resultado;
+
+    setResultado(resultado - numeroDigitado);
+
+    listaulli.splice(0, qualquer.length);
+
+    listaulli.push(valoranterior, " - ", numeroDigitado, " = ", resultado);
+
+    document
+      .getElementById("listahistorico")
+      .appendChild(CriaUlLI(listadehistorico[0]));
+  }
+
+  function multiplicar() {
+    var numeroDigitado = 0;
+
+    var valorResultadoanterior = resultado;
+
+    setResultado(resultado * numeroDigitado);
+
+    listaulli.splice(0, qualquer.length);
+
+    listaulli.push(valoranterior, " x ", numeroDigitado, " = ", resultado);
+
+    document
+      .getElementById("listahistorico")
+      .appendChild(CriaUlLI(listadehistorico[0]));
+  }
+
+  function dividir() {
+    var numeroDigitado = 0;
+
+    if (numeroDigitado == 0) {
+      numeroDigitado = 0;
+
+      setResultado(numeroDigitado);
+
+      listaulli.splice(0, qualquer.length);
+
+      listaulli.push(valoranterior, " / ", numeroDigitado, " = ", resultado);
+
+      document
+        .getElementById("listahistorico")
+        .appendChild(CriaUlLI(listadehistorico[0]));
+    } else {
+      setResultado(resultado / numeroDigitado);
+
+      listaulli.splice(0, qualquer.length);
+
+      listaulli.push(valoranterior, " / ", numeroDigitado, " = ", resultado);
+
+      document
+        .getElementById("listahistorico")
+        .appendChild(CriaUlLI(listadehistorico[0]));
+    }
+  }
+
+  function zerarresultado() {
+    setResultado(0);
   }
 
   return (
-    <div className="contador">
-      {contador}
+    <div className="calculadora">
       <div>
-          <button onClick={subtarir}>Menos</button>
-          <h1>Sistema de Calculadora</h1>
-          
-          <input type="text" id="caixanumerodigitado" name="caixanumerodigitado" size="1" value="0">
-          <br> </br>
+        <button onClick={somar}>Somar (+)</button>
+        <button onClick={diminuir}>Diminuir (-)</button>
+        <button onClick={multiplicar}>Multiplicar (x)</button>
+        <button onClick={dividir}>Dividir (/)</button>
 
-          <button onClick={somar}>Somar (+)</button>
-          <button onClick={diminuir}>Diminuir (-)</button>
-          <button onClick={multiplicar}>Multiplicar (x)</button>
-          <button onClick={dividir}>Dividir (/)</button>
+        <p>{Resultado}</p>
 
-          <h3>Resultado:</h3>
-          <p id="resultado">0</p>
-
-          <button onclick="zerarresultado()">Zerar Resultado</button>
-          <br> </br>
+        <button onClick={zerarresultado}>Zerar Resultado</button>
+        <br> </br>
 
         <div id="listahistorico"></div>
       </div>
