e8080b6415f23b5da0c892610984c700e12a28eb
Fernando Mette <metteinfor@gmail.com>
2021-12-07 18:23:08 -0300

    Adicionado mais contador

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Contador from './components/contador';
-import Calculadora from './components/Calculadora';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Contador from "./components/contador";
+import Calculadora from "./components/Calculadora";
 
 ReactDOM.render(
-    <div>
-      <Calculadora />
-      <Contador />
-    </div>,
-  document.getElementById('root')
+  <div>
+    <Calculadora />
+    <Contador />
+    <Contador />
+  </div>,
+  document.getElementById("root")
 );
