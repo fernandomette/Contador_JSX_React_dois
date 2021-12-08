e4b9f861b830c18d07098d2e645b32d77db1eaee
Fernando Mette <metteinfor@gmail.com>
2021-12-08 20:42:25 -0300

    Comitao

---
+++ src/components/tarefas/index.jsx
@@ -0,0 +1,26 @@
+import "./index.css";
+import { useEffect, useState } from "react";
+
+export default function Tarefas({ Responsavel }) {
+  const [Listatarefas, setListatarefa] = useState(["Passear Cao"]);
+  const [NovaTarefa, setNovaTarefa] = useState("");
+
+  function inputNovaTarefa(valorrecebido) {
+    console.log(valorrecebido.target.value);
+    setNovaTarefa(valorrecebido.target.value);
+  }
+
+  function adicionarListaTarefa() {
+    setListatarefa([...Listatarefas, NovaTarefa]);
+  }
+
+  return (
+    <div className="Listatarefas">
+      <h1>{Responsavel}</h1>
+      <input type="text" onChange={inputNovaTarefa} />
+      <button onClick={adicionarListaTarefa}>Adicionar</button>
+
+      <h1>{Listatarefas}</h1>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import Contador from "./components/contador";
+import Tarefas from "./components/tarefas";
 import Calculadora from "./components/Calculadora";
 
 ReactDOM.render(
   <div>
     <Calculadora />
+    <Tarefas Responsavel="Teste1" />
   </div>,
   document.getElementById("root")
 );
