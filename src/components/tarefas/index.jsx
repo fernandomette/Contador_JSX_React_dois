7eddd849056171afd4937d1e0f275cce02a8b8df
Fernando Mette <metteinfor@gmail.com>
2022-01-24 18:00:41 -0300

    tesste

--- src/components/tarefas/index.jsx
+++ src/components/tarefas/index.jsx
@@ -1,29 +1,45 @@
 import "./index.css";
 import { useEffect, useState } from "react";
+import React from 'react';
 
 
 export default function Tarefas({ Responsavel }) {
   const [ListTask, setListTask] = useState([""]);
   const [TextFild, setTextFild] = useState("");
 
+  class HelloForm extends React.Component {
+  state = {value: ''}
+
+  handleTextChange(e) {
+    this.setState({
+      texto: e.target.value
+    });
+  }
+
+  handleClick() {
+    alert(this.state.texto);
+  }
+
+  }
+
   function inputNewTask(value) {
-   console.log(value.target.value);
    setTextFild(value.target.value);
   }
 
   function addListTask() {
    setListTask([...ListTask, TextFild]);
-   setTextFild("");
    console.log(TextFild);
   }
 
-  return (
-    <div className="ListTask">
-      <h1>{Responsavel}</h1>
-      <input Value={TextFild} type="text" onChange={inputNewTask} />
-      <button onClick={addListTask}>Adicionar</button>
-
+  return() (
+    <div className="ListTask">    
+      <h1>Resposavel: {Responsavel}</h1>
+      Digite o texto: <input type="text" onChange={this.handleTextChange.bind(this)}></input>
+      <div>{this.state.texto}</div>
+      <button onClick={this.handleClick.bind(this)}>Exibir alerta</button>
+            
       <h1>{ListTask}</h1>
+
     </div>
   );
 }
