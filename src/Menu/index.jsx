1ea009c6ffd9e52f5db13f5cd8cab58ae6cea184
Fernando Mette <metteinfor@gmail.com>
2022-01-26 11:16:03 -0300

    react add

--- src/Menu/index.jsx
+++ src/Menu/index.jsx
@@ -1,10 +1,13 @@
 import "./index.css";
 import React from "react";
-import Tasks from "../components/Tasks";
 import { useState } from "react";
+import Tasks from "../components/Tasks";
+import PropTask from "../components/Tasks/PropTask"
+
 import AddTask from "../components/Tasks/AddTask";
 
 const Menu = () => {
+  
   const [tasks, settasks] = useState([
     {
       id: '1',
@@ -17,13 +20,46 @@ const Menu = () => {
       completed: false,
     },
   ]);
-    
-  
+
+  /** 
+  const handleTaskClick = (taskId) => {
+		const newTasks = tasks.map((task) => {
+			if (task.id === taskId) return { ...task, completed: !task.completed };
+
+			return task;
+		});
+
+		setTasks(newTasks);
+	};
+
+  */
+
+  const handleTaskAddition = (taskTitle) => {
+		const newTasks = [
+			...tasks,
+			{
+				title: taskTitle,
+				id: Math.random(10),
+				completed: false,
+			},
+		];
+
+		settasks(newTasks);
+	};
+
+  /**
+	const handleTaskDeletion = (taskId) => {
+		const newTasks = tasks.filter((task) => task.id !== taskId);
+
+		setTasks(newTasks);
+	};
+      
+  */
   return (
     <div className="container"> 
 
       <h1>Minhas Tarefas</h1>
-      <AddTask />
+      <AddTask handleTaskAddition={handleTaskAddition}/>
       <Tasks tasks={tasks}/>
 
     </div>
--- src/components/Tasks/AddTask.jsx
+++ src/components/Tasks/AddTask.jsx
@@ -1,16 +1,36 @@
 import React from "react";
+import { useState } from "react";
+
 import "./AddTask.css";
 import "./button";
 import Button from "./button";
 
-const AddTask = () => {
+const AddTask = ({ handleTaskAddition }) => {
+    const [inputData, SetInputData] = useState("");
+
+    const handleInputChange = (e) => {
+        setInputData(e.target.value);
+    };
+
+    const handleAddTaskClick = () => {
+        handleTaskAddition(inputData);
+        setInputData("");
+    }; 
 
     return (
         <div className="add-task-container">
-            <input className="add-task-input" type="text" />
-            <div className=".add-task-button-container"><Button>Adicionar</Button></div>
+            <input
+                onChange={handleInputChange}
+                value={inputData}
+                className="add-task-input"
+                type="text"
+            />
+            ;
+            <div className="add-task-button-container">
+                <Button onClick={handleAddTaskClick}>Adicionar</Button>
+            </div>
         </div>
     );
-}
+};
 
 export default AddTask;
